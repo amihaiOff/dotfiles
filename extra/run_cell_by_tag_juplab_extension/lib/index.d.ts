97d32beb9c06c5242216a316ecadc872b4a5dfea
amihai <amihai.offenbacher@voyantis.ai>
2024-02-24 11:26:27 +0200

    restructure files

---
+++ extra/run_cell_by_tag_juplab_extension/lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the run-tag-cells-jlab extension.
+ */
+declare const extension: JupyterFrontEndPlugin<void>;
+export default extension;
