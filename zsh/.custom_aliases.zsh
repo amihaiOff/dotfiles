51d99a1f0e3f6e3cf73c1e62345a344be2fc661d
amihai <amihai.offenbacher@voyantis.ai>
2024-08-21 15:16:04 +0300

    add stuff

--- zsh/.custom_aliases.zsh
+++ zsh/.custom_aliases.zsh
@@ -47,20 +47,68 @@ print_csv(){
 
 
 dbt_run() {
+    # Display help message
     if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-        echo "Usage: [query model name] [customer] [product] [version]"
+        echo "Usage: dbt_run -m <model_name> -c <customer> -p <product> -v <version> [-e <extra_args>] [-s <start_date>] [-d <end_date>]"
         return 0
     fi
 
-    local param1="$2"
-    local param2="$3"
+    # Initialize variables
+    local model2run=""
+    local customer=""
+    local product=""
+    local norm_version=""
+    local extra_args=""
+    local landing_start_date=""
+    local landing_end_date=""
 
-    # Convert to uppercase
-    p2=$(echo "$param2" | tr '[:lower:]' '[:upper:]')
-    p3=$(echo "$param3" | tr '[:lower:]' '[:upper:]')
+    # Parse named options
+    while getopts ":m:c:p:v:e:s:d:" opt; do
+        case $opt in
+            m) model2run="$OPTARG" ;;
+            c) customer=$(echo "$OPTARG" | tr '[:lower:]' '[:upper:]') ;;
+            p) product=$(echo "$OPTARG" | tr '[:lower:]' '[:upper:]') ;;
+            v) norm_version="$OPTARG" ;;
+            e) extra_args="$OPTARG" ;;
+            s) landing_start_date="$OPTARG" ;;
+            d) landing_end_date="$OPTARG" ;;
+            \?)
+                echo "Invalid option: -$OPTARG" >&2
+                return 1
+                ;;
+            :)
+                echo "Option -$OPTARG requires an argument." >&2
+                return 1
+                ;;
+        esac
+    done
+
+    # Check required arguments
+    if [[ -z "$model2run" || -z "$customer" || -z "$product" || -z "$norm_version" ]]; then
+        echo "Error: Model name, customer, product, and version are required."
+        return 1
+    fi
+
+    # Prepare vars
+    vars="{\"CUSTOMER_DWH\":\"CUSTOMER_PROD_$customer_$product\", \"CUSTOMER_DL\":\"CUSTOMER_PROD_$customer_$product\", \"CUSTOMER\": \"$customer\", \"PRODUCT\": \"$product\", \"VERSION\": \"$norm_version\"}"
+
+    # Add landing_start_date if provided
+    if [ -n "$landing_start_date" ]; then
+        vars=$(echo $vars | sed 's/}$/,"LANDING_START_DATE":"'$landing_start_date'"}/')
+    fi
+
+    # Add landing_end_date if provided
+    if [ -n "$landing_end_date" ]; then
+        vars=$(echo $vars | sed 's/}$/,"LANDING_END_DATE":"'$landing_end_date'"}/')
+    fi
 
     export HTTPS_PROXY="http://snowflake-proxy.internal.voyantis.io:8448"
-    dbt run -s $1 $5 --vars '{"CUSTOMER_DWH":"CUSTOMER_PROD_$p2_$p3", "CUSTOMER_DL":"CUSTOMER_PROD_$p2_$p3", "CUSTOMER": "$p2", "PRODUCT": "$p3", "VERSION": $4}'
+
+    if [ -n "$extra_args" ]; then
+       dbt run -s $model2run $extra_args --vars "$vars" 
+    else
+       dbt run -s $model2run --vars "$vars" 
+    fi
     unset HTTPS_PROXY
 }
 
